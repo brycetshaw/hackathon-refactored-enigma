1eaf3bfd2e5cc152ac445e9a46549a7784b1c1a6
PeiyunZhao <thomaszhao1211@gmail.com>
2021-02-13 17:06:35 -0700

    API Query Added

--- api/apiLocal.py
+++ api/apiLocal.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-import numpy as np
 import pickle
+import numpy as np
+import datetime
 import pandas as pd
 
 app = FastAPI()
-pickle_in = open("/Users/thomaszhao/Dev/FastAPI-main/classifier.pkl","rb")
+df = pd.read_csv("hackathon-refactored-enigma/data/parsed/dataframe2.csv",parse_dates=['Date_Time'])
 model=pickle.load(pickle_in)
 
 @app.get("/")
@@ -13,11 +14,18 @@ def health():
     return {"health": "OK"}
 
 
-@app.get("/predict/{csv_file}")
-def predict(csv_file):
+@app.get("/predict/")
+def predict(date: datetime.date):
+
+    return 
 
 
-    return {csv_file}
+@app.get("/query/")
+def predict(feature: str, start_date: str,end_date: str):
+    temp= (df['Date_Time']>datetime.datetime.strptime(start_date, '%Y-%m-%d')) & (df['Date_Time']<datetime.datetime.strptime(end_date, '%Y-%m-%d'))
+    result=df.loc[temp].set_index('Date_Time')[feature]
+    resultcsv=result
+    return result
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8000)
