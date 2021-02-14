c1a4c5610db60c58557971db0138b045acd1c925
Bryce Shaw <bryceshaw@ip-192-168-0-17.us-west-2.compute.internal>
2021-02-13 17:58:37 -0700

    add some docker things

---
+++ api/app/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import uvicorn
+import pickle
+import numpy as np
+import datetime
+import pandas as pd
+
+app = FastAPI()
+# pickle_in = open("/Users/thomaszhao/Dev/FastAPI-main/classifier.pkl","rb")
+df = pd.read_csv("dataframe2.csv",parse_dates=['Date_Time'])
+# model=pickle.load(pickle_in)
+
+@app.get("/")
+def health():
+    return {"health": "OK"}
+
+
+@app.get("/predict/")
+def predict(date: datetime.date):
+
+    return 
+
+
+@app.get("/query/")
+def predict(feature: str, start_date: str,end_date: str):
+    temp= (df['Date_Time']>datetime.datetime.strptime(start_date, '%Y-%m-%d')) & (df['Date_Time']<datetime.datetime.strptime(end_date, '%Y-%m-%d'))
+    result=df.loc[temp].set_index('Date_Time')[feature]
+    resultcsv=result
+    return result
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=4000)
